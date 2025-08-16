7ff0a68b7af5b7d5d5c86d9d434a7e661986f822
xiexian8888 <Xiexian7777@gmail.com>
2025-08-16 18:02:18 +0800

    Update config.js
    编码问题

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -20,10 +20,10 @@ const CONFIG = {
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '分享编程',
   HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'xiexian.us.kg'，  
+  HEO_HERO_TITLE_3: 'xiexian.us.kg', 
   HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题'，  
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com'，  
+  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
+  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
